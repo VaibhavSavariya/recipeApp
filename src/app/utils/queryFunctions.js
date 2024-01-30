673d332adf8cb713071f7f202b7bad68a0b217ac
VaibhavSavariya <vaibhavsavariya4@gmail.com>
2024-01-30 15:50:48 +0530

    server actions watching third time

--- src/app/utils/queryFunctions.js
+++ src/app/utils/queryFunctions.js
@@ -1,6 +1,8 @@
+import axios from "axios";
 import recipes from "../axios/Services/recipes";
 
 export const getRandomRecipesData = async () => {
+  "use server";
   try {
     const res = await recipes.getRandomRecipes();
     return res?.data;
@@ -8,3 +10,11 @@ export const getRandomRecipesData = async () => {
     return error;
   }
 };
+// export const getRandomRecipesData = async () => {
+//   try {
+//     const res = await axios.get("http://localhost:8080/users");
+//     return res?.data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
