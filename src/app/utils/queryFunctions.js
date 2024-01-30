c4de522b5e769f45276fcd732d8841cf3bf70ea7
VaibhavSavariya <vaibhavsavariya4@gmail.com>
2024-01-30 15:52:37 +0530

    server actions watching fourth time

--- src/app/utils/queryFunctions.js
+++ src/app/utils/queryFunctions.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import recipes from "../axios/Services/recipes";
 
 export const getRandomRecipesData = async () => {
-  "use server";
   try {
     const res = await recipes.getRandomRecipes();
     return res?.data;
